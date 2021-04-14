c18f6683aa87e60f74ddf600284b29f298f874ee
anca6889 <guth.jeanmaxime@gmail.com>
2021-04-14 10:59:45 +0200

    Test JS et Jquerry

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,5 +1,30 @@
-let question = document.querySelector("#text_field")
+alert("Grandpy se fait vieux doucement...Il ne vous reconnait plus très bien, il vous prend pour une personne s'appelant 'Billy' et il vous appelera toujours 'fiston'... Soyez indulgent envers lui ;-)");
+
+let question = document.querySelector("#text_field") // sélectionne le formulaire
+
 question.addEventListener("submit", function (event) {
     event.preventDefault();
-    console.log("Vous avez bien appuyé sur envoyer");
+    console.log("texte envoyé");
+
+    fetch("/ajax", {
+        method: "POST",
+        body: new FormData(question)
+    }); // récupère le text écrit dans la fenètre.
 })
+
+///////////////////////////////////////////////////////////////
+
+// partie écrit en Jquery le chat (ajoute le texte écrit)
+$(function () {
+
+    var $list, $text_field;
+    $list = $('ul'); // sélectionne la liste à puce
+    $text_field = $('#text_field'); // sélectionne le formulaire
+
+    $text_field.on('submit', function (e) { //e = évenement
+        e.preventDefault();
+        var text = $('input:text').val();
+        $list.append('<li>' + text + '</li>');
+        $('input:text').val('');
+    });
+});
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+""" This module will lauch the app """
+
+# !/usr/bin/python3
+# -*- coding: Utf-8 -*
 from app import app
 
 if __name__ == '__main__':
